41288599851f48156c06e49f20f50e2616439eda
FR_naoki <naoki.maekawa.511@gmail.com>
2024-08-23 23:57:51 +0900

    8/23

--- section2/form/input_7.php
+++ section2/form/input_7.php
@@ -6,28 +6,28 @@ header('X-FRAME-OPTIONS:DENY');
 
 // スーパーグローバル変数 php 9種類
 // 連想配列
-if(!empty($_POST)){
+if (!empty($_POST)) {
   echo '<pre>';
-  var_dump($_POST) ;
+  var_dump($_POST);
   echo '</pre>';
 }
 
 function h($str)
 {
-	return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
 
-// 入力、確認、完了 input.php, confirm.php, thanks.php
-// CSRF 偽物のinput.php->悪意のあるページ
-// input.php
+// 入力、確認、完了 input_7.php, confirm.php, thanks.php
+// CSRF 偽物のinput_7.php->悪意のあるページ
+// input_7.php
 
 $pageFlag = 0;
 
-if(!empty($_POST['btn_confirm'])){
+if (!empty($_POST['btn_confirm'])) {
   $pageFlag = 1;
 }
-if(!empty($_POST['btn_submit'])){
+if (!empty($_POST['btn_submit'])) {
   $pageFlag = 2;
 }
 
@@ -36,87 +36,97 @@ if(!empty($_POST['btn_submit'])){
 
 <!DOCTYPE html>
 <meta charset="utf-8">
+
 <head></head>
+
 <body>
 
 
-<?php if($pageFlag === 1 ) : ?>
-<?php if($_POST['csrf'] === $_SESSION['csrfToken']) :?>
-<form method="POST" action="input.php">
-氏名
-<?php echo h($_POST['your_name']) ;?>
-<br>
-メールアドレス
-<?php echo h($_POST['email']) ;?>
-<br>
+  <?php if ($pageFlag === 1) : ?>
+    <?php if ($_POST['csrf'] === $_SESSION['csrfToken']) : ?>
+      <form method="POST" action="input_7.php">
+        氏名
+        <?php echo h($_POST['your_name']); ?>
+        <br>
+        メールアドレス
+        <?php echo h($_POST['email']); ?>
+        <br>
 
-<input type="submit" name="back" value="戻る">
-<input type="submit" name="btn_submit" value="送信する">
-<input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']) ;?>">
-<input type="hidden" name="email" value="<?php echo h($_POST['email']) ;?>">
-<input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']) ;?>">
-</form>
+        <input_7 type="submit" name="back" value="戻る">
+          <input_7 type="submit" name="btn_submit" value="送信する">
+            <input_7 type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+              <input_7 type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
+                <input_7 type="hidden" name="csrf" value="<?php echo h($_POST['csrf']); ?>">
+      </form>
 
-<?php endif; ?>
+    <?php endif; ?>
 
-<?php endif; ?>
+  <?php endif; ?>
 
-<?php if($pageFlag === 2 ) : ?>
-<?php if($_POST['csrf'] === $_SESSION['csrfToken']) :?>
-送信が完了しました。
+  <?php if ($pageFlag === 2) : ?>
+    <?php if ($_POST['csrf'] === $_SESSION['csrfToken']) : ?>
+      送信が完了しました。
 
-<?php unset($_SESSION['csrfToken']); ?>
-<?php endif; ?>
-<?php endif; ?>
+      <?php unset($_SESSION['csrfToken']); ?>
+    <?php endif; ?>
+  <?php endif; ?>
 
 
-<?php if($pageFlag === 0 ) : ?>
-<?php
-if(!isset($_SESSION['csrfToken'])){
-  $csrfToken = bin2hex(random_bytes(32));
-  $_SESSION['csrfToken'] = $csrfToken;
-}
-$token = $_SESSION['csrfToken'];
-?>
+  <?php if ($pageFlag === 0) : ?>
+    <?php
+    if (!isset($_SESSION['csrfToken'])) {
+      $csrfToken = bin2hex(random_bytes(32));
+      $_SESSION['csrfToken'] = $csrfToken;
+    }
+    $token = $_SESSION['csrfToken'];
+    ?>
 
-<form method="POST" action="input.php">
-氏名
-<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']) ;} ?>">
-<br>
-メールアドレス
-<input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']) ;} ?>">
-<br>
-ホームページ
-<input type="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']) ;} ?>">
-<br>
-性別
-<input type="radio" name="gender" value="0">男性
-<input type="radio" name="gender" value="1">女性
-<br>
-年齢
-<select name="age">
-  <option value="">選択してください</option>
-  <option value="1">〜19歳</option>
-  <option value="2">20歳〜29歳</option>
-  <option value="3">30歳〜39歳</option>
-  <option value="4">40歳〜49歳</option>
-  <option value="5">50歳〜59歳</option>
-  <option value="6">60歳〜</option>
-</select> 
-<br>
-お問い合わせ内容
-<textarea name="contact">
-<?php if(!empty($_POST['contact'])){echo h($_POST['contact']) ;} ?>
+    <form method="POST" action="input_7.php">
+      氏名
+      <input_7 type="text" name="your_name" value="<?php if (!empty($_POST['your_name'])) {
+                                                      echo h($_POST['your_name']);
+                                                    } ?>">
+        <br>
+        メールアドレス
+        <input_7 type="email" name="email" value="<?php if (!empty($_POST['email'])) {
+                                                    echo h($_POST['email']);
+                                                  } ?>">
+          <br>
+          ホームページ
+          <input_7 type="url" name="url" value="<?php if (!empty($_POST['url'])) {
+                                                  echo h($_POST['url']);
+                                                } ?>">
+            <br>
+            性別
+            <input_7 type="radio" name="gender" value="0">男性
+              <input_7 type="radio" name="gender" value="1">女性
+                <br>
+                年齢
+                <select name="age">
+                  <option value="">選択してください</option>
+                  <option value="1">〜19歳</option>
+                  <option value="2">20歳〜29歳</option>
+                  <option value="3">30歳〜39歳</option>
+                  <option value="4">40歳〜49歳</option>
+                  <option value="5">50歳〜59歳</option>
+                  <option value="6">60歳〜</option>
+                </select>
+                <br>
+                お問い合わせ内容
+                <textarea name="contact">
+<?php if (!empty($_POST['contact'])) {
+      echo h($_POST['contact']);
+    } ?>
 </textarea>
-<br>
-<input type="checkbox" name="caution" value="1">注意事項にチェックする
-<br>
+                <br>
+                <input_7 type="checkbox" name="caution" value="1">注意事項にチェックする
+                  <br>
 
-<input type="submit" name="btn_confirm" value="確認する">
-<input type="hidden" name="csrf" value="<?php echo $token; ?>">
-</form>
-<?php endif; ?>
+                  <input_7 type="submit" name="btn_confirm" value="確認する">
+                    <input_7 type="hidden" name="csrf" value="<?php echo $token; ?>">
+    </form>
+  <?php endif; ?>
 
 </body>
-</html>
 
+</html>
