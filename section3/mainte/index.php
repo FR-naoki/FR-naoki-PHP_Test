f0881386c6b423f4aa5a64a70a1c14e1ee68e0e1
FR_naoki <naoki.maekawa.511@gmail.com>
2024-09-10 20:07:27 +0900

    9/10

--- section3/mainte/index.php
+++ section3/mainte/index.php
@@ -1,9 +1,9 @@
 <?php
 
-require 'db_connection.php';
+require 'db_connection_2.php';
 
 // ユーザー入力なし query
-// $sql = 'select * from contacts where id = 4'; //sql
+// $sql = 'select * from contacts where id = 2'; //sql
 // $stmt = $pdo->query($sql); //sql実行 ステートメント
 
 // $result = $stmt->fetchall();
@@ -15,7 +15,7 @@ require 'db_connection.php';
 // ユーザー入力あり prepare, bind, execute 悪意ユーザ delete * SQLインジェクション対策
 $sql = 'select * from contacts where id = :id'; //名前付きプレースホルダ
 $stmt = $pdo->prepare($sql); //プリペアードステートメント
-$stmt->bindValue('id', 5, PDO::PARAM_INT); //紐付け
+$stmt->bindValue('id', 3, PDO::PARAM_INT); //紐付け
 $stmt->execute(); //実行
 
 $result = $stmt->fetchall();
